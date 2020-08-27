b92fb82a3db0c3ca10d70b75a294bdb26c080b44
SumedhZope <sumedhzope@gmail.com>
2020-08-27 08:50:31 +0530

    Added models-Group,Posts

--- IITBHUForums/forum/admin.py
+++ IITBHUForums/forum/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post,Group
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Group)
--- IITBHUForums/forum/models.py
+++ IITBHUForums/forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    created_by = models.TextField(default='')
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    created_by = models.DateField(auto_now_add=True)
+    created_by = models.TextField(default=' ')
+    group =  models.ForeignKey(Group,on_delete=models.CASCADE,null=True,blank=True)
+    def __str__(self):
+        return self.title
