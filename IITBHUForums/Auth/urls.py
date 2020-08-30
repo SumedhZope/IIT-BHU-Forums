1c84799ccfef1cb90c552ebb575d6f77395b2c7c
YashGoyal28 <y.g.2001yashgoyal@gmail.com>
2020-08-30 15:04:28 +0530

    basic login and register

--- IITBHUForums/Auth/urls.py
+++ IITBHUForums/Auth/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('login/', views.loginpage),
+    path('register/',views.register),
 ]
--- IITBHUForums/forum/views.py
+++ IITBHUForums/forum/views.py
@@ -1,31 +1,5 @@
 from django.shortcuts import render,redirect
-from .forms import CreateNewUserForm
 from django.contrib.auth import authenticate,login
 
-def loginpage(request, *args, **kwargs):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username,password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            print("Error")
-            return render(request, 'login.html')
-    return render(request, 'login.html')
-                
-
-
-def register(request,*args,**kwargs):
-    form = CreateNewUserForm()
-    if request.method == "POST":
-        form = CreateNewUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
-        else:
-            print("Error")
-            return render(request, 'register.html',{'form' : form})
-    return render(request, 'register.html',{'form' : form})
-
+def home(request, *args, **kwargs):
+    return render(request,'home.html',{'user' : request.user})
