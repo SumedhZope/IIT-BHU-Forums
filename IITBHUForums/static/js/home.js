5161ef87f32936f772a370fb9d98eeaac5aa9167
YashGoyal28 <y.g.2001yashgoyal@gmail.com>
2020-09-02 15:01:10 +0530

    fixed reponsiveness mistakes

--- IITBHUForums/static/js/home.js
+++ IITBHUForums/static/js/home.js
@@ -6,29 +6,7 @@ $(window).resize(function(){
 });
 
 function resposive(){
-    var h = $(window).height()-$('.header').height();
-    //console.log(h);
-   // $('.front').css({'height':h+'px'});
-    if(screen.width<800){
-        $('.content').css({'display':'block'});
-        $('.loading_img').css({'width':'70%'});
-        $('.logo').css({'max-height':'150px'});
-        $('.sign').css({'height':'130px'});
-        $('.sign').css({'font-size':'40px'});
-        $('.content_child').css({'width':'100%'});
-    }else if(screen.width<1415){
-        $('.content').css({'display':'flex'});
-        $('.loading_img').css({'width':'80%'});
-        $('.logo').css({'max-height':'100px'});
-        $('.sign').css({'height':'100px'});
-        $('.sign').css({'font-size':'30px'});
-        $('.content_child').css({'width':'50%'});
-    }else{
-        $('.content').css({'display':'flex'});
-        $('.loading_img').css({'width':'60%'});
-        $('.logo').css({'max-height':'100px'});
-        $('.sign').css({'height':'100px'});
-        $('.sign').css({'font-size':'30px'});
-        $('.content_child').css({'width':'50%'});
-    }
+    var h = $(window).height()-$('.header').height()-$('.front').height();
+    h /= 2;
+    $('.front').css({'padding-top':h+'px'});
 }
