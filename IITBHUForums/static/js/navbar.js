afe289732b920cdef046dc8fa83ec65f106f6fb7
SumedhZope <sumedhzope@gmail.com>
2020-09-03 14:10:06 +0530

    Close Navbar when clicked outside

--- IITBHUForums/static/js/navbar.js
+++ IITBHUForums/static/js/navbar.js
@@ -14,7 +14,7 @@ const dropdownUser = () =>{
     const triangle = document.querySelector('.triangle');
     const user = document.querySelector('.user');
     const profile_picture = document.querySelector('.profile-picture');
-    const dropdown = document.querySelector('.dropdown')
+    const dropdown = document.querySelector('.dropdown');
     triangle.addEventListener('click',() => {
         dropdown.classList.toggle('dropdown-active');
         triangle.classList.toggle('triangle-active');
@@ -39,6 +39,17 @@ const searchbar = () => {
         search.classList.toggle('search-active');
     });
 
+}
+
+window.onclick = function(event){
+    const triangle = document.querySelector('.triangle');
+    const dropdown = document.querySelector('.dropdown');
+    const dropdown_main_div = document.querySelector('.ul-2');
+    console.log(event.target.parentNode);
+    if(event.target != dropdown && event.target.parentNode != dropdown && event.target != dropdown_main_div && event.target.parentNode != dropdown_main_div){
+        triangle.classList.remove('triangle-active');
+        dropdown.classList.remove('dropdown-active');
+    }
 }
 
 const process = () =>{
