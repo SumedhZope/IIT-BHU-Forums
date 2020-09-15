45614258a5f059602e417795e7ca76c4f941d20b
YashGoyal28 <y.g.2001yashgoyal@gmail.com>
2020-09-15 12:52:54 +0530

    remove garbage

--- IITBHUForums/Auth/views.py
+++ IITBHUForums/Auth/views.py
@@ -5,40 +5,6 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 
-#def loginpage(request, *args, **kwargs):
-#    if(request.user.is_authenticated):
-#        return redirect('/')
-#    if request.method == "POST":
-#        username = request.POST.get('username')
-#        password = request.POST.get('password')
-#        user = authenticate(request, username=username, password=password)
-#        if user is not None:
-#            login(request, user)
-#            return redirect('/')
-#        else:
-#            print("Error")
-#            return render(request, 'login.html')
-#    return render(request, 'login.html')
-#
-#
-#def register(request, *args, **kwargs):
-#    if(request.user.is_authenticated) :
-#        return redirect('/')
-#    form = CreateNewUserForm()
-#    if request.method == "POST":
-#        form = CreateNewUserForm(request.POST)
-#        if form.is_valid():
-#            form.save()
-#            username = request.POST.get('username')
-#            password = request.POST.get('password1')
-#            user = authenticate(request, username=username, password=password)
-#            login(request, user)
-#            return redirect('/')
-#        else:
-#            print("Error")
-#            return render(request, 'register.html', {'form' : form})
-#    return render(request, 'register.html', {'form' : form})
-
 
 def landingpage(request,*args,**kwargs):
     if request.method == 'POST':
