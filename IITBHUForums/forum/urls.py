0d0be8a85bdd69b328d6f3a7a3034cb28e1858b9
SumedhZope <sumedhzope@gmail.com>
2020-09-19 11:26:26 +0530

    Update urls.py

--- IITBHUForums/forum/urls.py
+++ IITBHUForums/forum/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.home, name="home"),
     path('testnav/', views.nav),
     path('profile/',views.profile)
 ]
