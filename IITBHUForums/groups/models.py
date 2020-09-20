cab116bdd2d1a87228776115fc17d60c144ccf4f
Neema-max <62277906+Neema-max@users.noreply.github.com>
2020-09-20 23:13:52 +0530

    App ,modles created and registered

---
+++ IITBHUForums/groups/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class group(models.Model):
+    name= models.CharField(max_length=100)
+    description= models.TextField()
+    group_icon=models.ImageField()
+    group_id=models.IntegerField(default=0)
