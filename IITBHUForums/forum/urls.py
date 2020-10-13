7ada9e1c54aced245996e729eef7641961e3f259
SumedhZope <sumedhzope@gmail.com>
2020-10-13 17:04:27 +0530

    Added posts

--- IITBHUForums/forum/urls.py
+++ IITBHUForums/forum/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('testnav/', views.nav, name='navbar'),
-    path('new_group/',views.submit_form,name="new_group")
+    path('new_group/',views.submit_form,name="new_group"),
+    path('new_post/', views.make_post)
 ]
